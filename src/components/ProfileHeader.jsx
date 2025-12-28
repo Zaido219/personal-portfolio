6a063ff0dcc0e8d07e48e1d632ec594af4abad44
Artifice-219 <johnphilliplormalbas@gmail.com>
2025-12-27 22:54:07 -0800

    feat:Initila designs for the header/hero section

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,24 +1,30 @@
 const ProfileHeader = () => {
     return(
-        <header>
-            <section id="Hero">
-                <div id="avatarWrapper">
+        <header className="flex">
+            <section className="flex items-center gap-8" id="Hero">
+                <div  className="rounded-full overflow-hidden w-60 h-60 bg-green-500 shrink-0 m-2" id="avatarWrapper">
                     {/* TODO 2 : Make sure to have an alt text in here  */}
-                    <img src="src/assets/images/image.png" alt="" />
+                    <img className="w-full h-full object-cover" src="src/assets/images/image.png" alt="" />
                 </div>
-                <div id="content">
+                <div className="" id="content">
                     <div className="profileInfo">
                         <div className="nameHeader">
                         <h1>John Phillip Lor</h1>
                         {/* That blue checkmark badge could go here */}
                         </div>
-                        <p className="location">Metro Manila, Philippines</p>
+                        <p className="location">Sapang Dayap, San Ildefonso Bulacan</p>
                         <p className="jobTitle">Software Engineering Intern</p>
                     </div>
                 </div>
             </section>
-            <section id="actions">
-
+            <section className="" id="actions">
+                <button>
+                    Email Me
+                </button>
+                <section className="" id="socials">
+                    <button>Github</button>
+                    <button>LinkedIn</button>
+                </section>
             </section>
         </header>
     );
