1ec3c111821a696396dbea74f37d001d4d5cd744
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-02-13 23:13:21 -0800

    style:Added new content for the header part

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -3,9 +3,19 @@ const ProfileHeader = () => {
         <header className="flex">
             <div>
                 {/* image goes here */}
+                <div className="image-container">
+                    <img src="src/assets/images/image.png" alt="my image" />
+                </div>
             </div>
             <div>
                 {/* rest of the content */}
+                <p>John Phillip Lor Malbas</p>
+                <p>San Ildefonso,Bulacan,Philippines</p>
+                <p>Student Intern at the GAD office of Bulacan Agricultural State College</p>
+                <div>
+                    {/* theme toggle button */}
+                    <button>Change theme</button>
+                </div>
                 <div>
                     {/* for the socials link */}
                 </div>
