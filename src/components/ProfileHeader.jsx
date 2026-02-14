2fbccc6fbc298674e6e229c85f005eba6f9619bc
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-02-13 23:48:41 -0800

    style:WIP saving progress

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,23 +1,29 @@
 const ProfileHeader = () => {
     return(
-        <header className="flex">
-            <div>
+        <header className="flex bg-red-500 w-fit p-10">
+            <div className="bg-orange-500">
                 {/* image goes here */}
-                <div className="image-container">
-                    <img src="src/assets/images/image.png" alt="my image" />
+                <div className="h-40 w-40 bg-yellow-500">
+                    <img className="h-10 w-10 bg-green-500" src="src/assets/images/image.png" alt="my image" />
                 </div>
             </div>
-            <div>
+            <div className="flex bg-blue-500 flex-col p-10">
                 {/* rest of the content */}
-                <p>John Phillip Lor Malbas</p>
-                <p>San Ildefonso,Bulacan,Philippines</p>
-                <p>Student Intern at the GAD office of Bulacan Agricultural State College</p>
-                <div>
+                <div className="bg-indigo-500">
+                    <p>John Phillip Lor Malbas</p>
+                    <p>San Ildefonso,Bulacan,Philippines</p>
+                </div>
+                <p>Student Intern</p>
+                <div className="bg-violet-500">
                     {/* theme toggle button */}
                     <button>Change theme</button>
                 </div>
-                <div>
+                <div className="bg-red-500">
                     {/* for the socials link */}
+                    <a href="">Facebook</a>
+                    <a href="">Github</a>
+                    <a href="">Tiktok</a>
+                    <a href="">LinkedIn</a>
                 </div>
             </div>
         </header>
