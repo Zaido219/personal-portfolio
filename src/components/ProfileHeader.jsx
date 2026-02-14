c084c19b88747b929e07e8587972cea4171f3c6d
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-02-13 23:04:42 -0800

    feat:Reverted everything caused we need to start again

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,31 +1,15 @@
 const ProfileHeader = () => {
     return(
         <header className="flex">
-            <section className="flex items-center gap-8" id="Hero">
-                <div  className="rounded-full overflow-hidden w-60 h-60 bg-green-500 shrink-0 m-2" id="avatarWrapper">
-                    {/* TODO 2 : Make sure to have an alt text in here  */}
-                    <img className="w-full h-full object-cover" src="src/assets/images/image.png" alt="" />
+            <div>
+                {/* image goes here */}
+            </div>
+            <div>
+                {/* rest of the content */}
+                <div>
+                    {/* for the socials link */}
                 </div>
-                <div className="" id="content">
-                    <div className="profileInfo">
-                        <div className="nameHeader">
-                        <h1>John Phillip Lor</h1>
-                        {/* That blue checkmark badge could go here */}
-                        </div>
-                        <p className="location">Sapang Dayap, San Ildefonso Bulacan</p>
-                        <p className="jobTitle">Software Engineering Intern</p>
-                    </div>
-                </div>
-            </section>
-            <section className="" id="actions">
-                <button>
-                    Email Me
-                </button>
-                <section className="" id="socials">
-                    <button>Github</button>
-                    <button>LinkedIn</button>
-                </section>
-            </section>
+            </div>
         </header>
     );
 };
