12797f343fa898cab551a8dd0fe02aedbf55aa2a
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-02-14 01:54:14 -0800

    style:Saving progress

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,33 +1,44 @@
 const ProfileHeader = () => {
-    return(
-        <header className="flex items-center gap-8 w-full">
-            <div className="bg-orange-500">
-                {/* image goes here */}
-                <div className="h-40 w-40">
-                    <img className="h-full w-full object-cover" src="src/assets/images/image.png" alt="my image" />
-                </div>
-            </div>
-            <div className="flex flex-col p-10">
-                {/* rest of the content */}
-                <div className="">
-                    <p className="font-bold text-xl">John Phillip Lor Malbas</p>
-                    <p className="font-medium">San Ildefonso,Bulacan,Philippines</p>
-                </div>
-                <p>Student Intern</p>
-                <div className="">
-                    {/* theme toggle button */}
-                    <button>Change theme</button>
-                </div>
-                <div className="">
-                    {/* for the socials link */}
-                    <a href="">Facebook</a>
-                    <a href="">Github</a>
-                    <a href="">Tiktok</a>
-                    <a href="">LinkedIn</a>
-                </div>
-            </div>
-        </header>
-    );
+  return (
+    <header className="flex items-start gap-4 w-full">
+      <div className="">
+        {/* image goes here */}
+        <div className="h-40 w-40">
+          <img
+            className="h-full w-full object-cover"
+            src="src/assets/images/image.png"
+            alt="my image"
+          />
+        </div>
+      </div>
+    <div className="flex flex-col bg-blue-500 gap-7">
+        {/* rest of the content */}
+        <div className="">
+            <p className="font-extrabold text-xl">John Phillip Lor Malbas</p>
+            <p className="font-bold">San Ildefonso,Bulacan,Philippines</p>
+        </div>
+
+        <p className="font-semibold">Student Intern</p>
+
+        <div className="flex gap-2 bg-blue-600">
+          {/* for the socials link */}
+          <a href="" className="flex items-center gap-1">
+            <i class="fa-brands fa-facebook-f"></i>
+            Facebook
+          </a>
+          <a href="" className="flex items-center gap-1">
+            <i class="fa-brands fa-github"></i>
+            Github</a>
+          <a href="" className="flex items-center gap-1">
+            <i class="fa-brands fa-tiktok"></i>
+            Tiktok</a>
+          <a href="" className="flex items-center gap-1">
+            <i class="fa-brands fa-linkedin"></i>
+            LinkedIn</a>
+        </div>
+    </div>
+    </header>
+  );
 };
 
 export default ProfileHeader;
