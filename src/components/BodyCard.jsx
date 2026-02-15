a73fe2bdd82a8e75488b69d72564e32832bf3002
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-02-14 21:33:03 -0800

    style:Saving changes

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -1,7 +1,7 @@
 const BodyCard = (props) => {
     return (
         /* Card Wrapper: Defines the size and style of the card */
-        <div className="max-w-5xl mx-auto my-10 p-8 bg-white rounded-2xl shadow-lg"> 
+        <div className="max-w-6xl mx-auto my-10 p-8 bg-white rounded-2xl shadow-lg"> 
             <div className="p-2">
                 {props.children}
             </div>
