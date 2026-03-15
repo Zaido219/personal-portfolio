38a082137005260b5460f6f33fa362ec4a9e220c
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-03-15 02:00:18 -0700

    style:Added initial structure for the recognition cards

---
+++ src/components/Recognitions.jsx
@@ -0,0 +1,12 @@
+import BaseCard from "./Card";
+const Recognitions = () => {
+  return (
+    <section>
+      <BaseCard>
+        <p>Recognition 1</p>
+      </BaseCard>
+    </section>
+  );
+};
+
+export default Recognitions;
