d26761d4604706a2412ea3729f1707a844b7340d
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-03-15 04:06:34 -0700

    Feat:Added SectionHeaderText base component for all section header texts

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,19 +1,22 @@
 import BaseCard from "./Ui/Card";
+import BaseSection from "./Ui/BaseSection";
 
 const ProjectCard = () => {
   return (
-    <section className="max-w-lg">
-      <BaseCard>
-        <div>
-          <h2>Recent Projects</h2>
-        </div>
-        <ul className="flex flex-col gap-2">
+    <BaseSection>
+      <section className="max-w-lg">
+        <BaseCard>
+          <div>
+            <h2>Recent Projects</h2>
+          </div>
+          <ul className="flex flex-col gap-2">
             <li>Project 1</li>
             <li>Project 2</li>
             <li>Project 3</li>
-        </ul>
-      </BaseCard>
-    </section>
+          </ul>
+        </BaseCard>
+      </section>
+    </BaseSection>
   );
 };
 
---
+++ src/components/Ui/SectionHeaderText.jsx
@@ -0,0 +1,9 @@
+const SectionHeader = ({title="Default Header Title"}) => {
+    return (
+        <header>
+            <h2> {title} </h2>
+        </header>
+    )
+}
+
+export default SectionHeader;
