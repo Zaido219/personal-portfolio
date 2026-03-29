33da7fb05fcf1f39331f0a065f52ddda50bdbfe4
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-03-29 03:08:19 -0700

    Style:Update style

--- src/components/Ui/ProjectItem.tsx
+++ src/components/Ui/ProjectItem.tsx
@@ -2,9 +2,9 @@ import {ProjectItemProps} from "../../interface/types"
 
 const ProjectItem = ({projectName, projectDescription, projectLink, ...props} : ProjectItemProps) => {
     return (
-        <div>
-            <p>{projectName}</p>
-            <p>{projectDescription}</p>
+        <div className="p-2 bg-gray-100 rounded-xl flex flex-col gap-1">
+            <strong>{projectName}</strong>
+            <p className="text-md">{projectDescription}</p>
             <div>
                 <a href={projectLink}></a>
             </div>
