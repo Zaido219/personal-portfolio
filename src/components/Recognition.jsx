78b9b50fd80f423df903acc40052b97aa8521c29
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-04-01 06:38:44 -0700

    style:Style updates

--- src/components/Recognition.jsx
+++ src/components/Recognition.jsx
@@ -1,15 +1,15 @@
 import BaseSection from "./Ui/BaseSection";
 import SectionHeader from "./Ui/SectionHeaderText";
+import CertificateItem from "./Ui/CertificateItem";
 
 const Recognition = () => {
     return (
         <BaseSection>
-        <SectionHeader
-            title="Recent Certifications"
-        />
-        <section>
-            {/* This should be a list of awards and certs */}
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat, libero.
+        <SectionHeader title="Recent Certifications"/>
+        <section className="flex flex-col gap-2 p-2">
+            <CertificateItem></CertificateItem>
+            <CertificateItem></CertificateItem>
+            <CertificateItem></CertificateItem>
         </section>
         </BaseSection>
     );
