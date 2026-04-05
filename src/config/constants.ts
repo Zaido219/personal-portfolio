1f709a80ec8d2207d42ebc32795e9026363cd4b0
Zaido <jpmalbas4@gmail.com>
2026-04-06 07:54:52 +0800

    feat:Added constants for the work experiences

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+const experiences = [
+    {title:'Sample Experience Title', company: 'Sample experience company', startYear: '2002', endYear: '2003'},
+    {title:'Sample Experience Title', company: 'Sample experience company', startYear: '2002', endYear: '2003'},
+    {title:'Sample Experience Title', company: 'Sample experience company', startYear: '2002', endYear: '2003'}
+]
