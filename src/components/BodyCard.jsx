fefdcbe38048b996320d9f7f92cc8bf986bee088
Zaido <jpmalbas4@gmail.com>
2026-04-06 14:29:29 +0800

    feat:WIP GalleryPhotoSection

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -1,8 +1,8 @@
 const BodyCard = (props) => {
     return (
         /* Card Wrapper: Defines the size and style of the card */
-        <div className="w-fit mx-auto my-10 p-5 shadow-lg flex justify-center rounded-sm"> 
-            <div className="p-2">
+        <div className="w-fit mx-auto my-10 p-4 shadow-lg flex justify-center rounded-sm"> 
+            <div className="p-1">
                 {props.children}
             </div>
         </div>
--- src/components/Ui/SectionHeaderText.jsx
+++ src/components/Ui/SectionHeaderText.jsx
@@ -1,6 +1,6 @@
 const SectionHeader = ({title="Default Header Title"}) => {
     return (
-        <header className="w-fit  p-2 m-2">
+        <header className="w-fit p-1 m-1">
             <h2 className="text-lg font-semibold tracking-tight"> {title} </h2>
         </header>
     )
