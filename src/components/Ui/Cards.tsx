e8fb8c894ced233232b3348a62214e0202645330
Zaido <jpmalbas4@gmail.com>
2026-04-06 10:23:06 +0800

    style:Updates on hover card component

--- src/components/Ui/Cards.tsx
+++ src/components/Ui/Cards.tsx
@@ -2,7 +2,7 @@ import type { HoverCardProps } from "../../interface/types"
 
 export const HoverCards = ({children, className}:HoverCardProps) => {
     return(
-        <div className={`hover:bg-blue-300 transition duration-200 ease-in-out hover:scale-105 ${className}`}>
+        <div className={`bg-gray-100 hover:bg-gray-300 transition duration-200 ease-in-out hover:scale-105 rounded-md ${className}`}>
             {children}
         </div>
     )
--- src/components/Ui/WorkExperienceItem.tsx
+++ src/components/Ui/WorkExperienceItem.tsx
@@ -1,17 +1,27 @@
-import {WorkExperienceItemProps} from "./../../interface/types"
+import { WorkExperienceItemProps } from "./../../interface/types";
+import { HoverCards } from "./Cards";
 
-const WorkExperienceItem = ({workTitle, workCompany, startDate, endDate}:WorkExperienceItemProps) =>{
-    return(
-        <div className="flex flex-col gap-1 p-1 rounded-md w-sm bg-gray-100">
-            <strong className="p-1 text-md">{workTitle}</strong>
-            <div className="flex justify-between gap-1 p-1">
-                <p className="text-sm">{workCompany}</p>
-                <div className="text-sm flex justify-between gap-2">
-                    <span>{startDate} - {endDate}</span>
-                </div>
-            </div>
+const WorkExperienceItem = ({
+  workTitle,
+  workCompany,
+  startDate,
+  endDate,
+}: WorkExperienceItemProps) => {
+  return (
+    <HoverCards>
+      <div className="flex flex-col gap-1 p-1 rounded-md w-sm">
+        <strong className="p-1 text-md">{workTitle}</strong>
+        <div className="flex justify-between gap-1 p-1">
+          <p className="text-sm">{workCompany}</p>
+          <div className="text-sm flex justify-between gap-2">
+            <span>
+              {startDate} - {endDate}
+            </span>
+          </div>
         </div>
-    );
+      </div>
+    </HoverCards>
+  );
 };
 
 export default WorkExperienceItem;
