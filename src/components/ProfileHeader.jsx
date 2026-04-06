b8512b4981b83c3096901764ef48824d6a64502a
Zaido <jpmalbas4@gmail.com>
2026-04-06 17:20:53 +0800

    Style:Style updates

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -50,29 +50,29 @@ const ProfileHeader = () => {
           </a>
         </nav>
       </section>
-      <section className="self-end flex gap-5 items-center p-1 ">
+      <section className="self-end flex flex-col w-fit gap-2 items-center p-1">
         {/* download resume aand email section */}
         <HoverCards>
-            <div className="flex p-2 gap-2 items-center">
-          <span>
-            <i class="fa-regular fa-file"></i>
-          </span>
-          <button className="p-1">My Resume</button>
-        </div>
+          <div className="flex p-2 gap-2 items-center w-md">
+            <span>
+              <i class="fa-regular fa-file"></i>
+            </span>
+            <button className="p-1">My Resume</button>
+          </div>
         </HoverCards>
         <HoverCards>
-          <div className="flex p-2 gap-2 items-center">
-          <span>
-            <i class="fa-regular fa-envelope"></i>
-          </span>
-          <button className="p-1">Email</button>
-        </div>
+          <div className="flex p-2 gap-2 items-center  w-md">
+            <span>
+              <i class="fa-regular fa-envelope"></i>
+            </span>
+            <button className="p-1">Email</button>
+          </div>
         </HoverCards>
         {/* display work experiences */}
         <HoverCards>
-          <div className="flex items-center work-experiences">
-          <button className="flex items-center p-2">Certifications</button>
-        </div>
+          <div className="flex items-center work-experiences  w-md">
+            <button className="flex items-center p-2">Certifications</button>
+          </div>
         </HoverCards>
       </section>
     </header>
