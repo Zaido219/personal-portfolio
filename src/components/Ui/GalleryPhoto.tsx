115f1c49aa34471445547e40f00b8cd1e58c57ec
Zaido <jpmalbas4@gmail.com>
2026-04-06 17:05:36 +0800

    Style:Style updates

--- src/components/Ui/GalleryPhoto.tsx
+++ src/components/Ui/GalleryPhoto.tsx
@@ -2,7 +2,7 @@ import { GalleryPhotoProps } from "../../interface/types";
 
 const GalleryPhotos = ({src, alt}:GalleryPhotoProps) => {
     return (
-        <div className="h-40 w-40">
+        <div className="h-45 w-45">
             <img className="h-full w-full object-cover" src={src} alt={alt} />
         </div>
     );
