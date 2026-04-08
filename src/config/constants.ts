4f922a42dbf9f1266d056bba39b7f37f8ddcc4b6
Zaido <jpmalbas4@gmail.com>
2026-04-08 08:24:57 +0800

    style:Updated portfolio styles

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -38,8 +38,8 @@ export const Testimonials : TestimonialProps[] = [
 ]
 
 export const workExperiences = [
-    {title:'BSIT Student', company: 'Bulacan Agricultural State College', startYear: '2002', endYear: '2003'},
-    {title:'Glass and Aluminum installation helper', company: 'Family Owned Small Business', startYear: '2002', endYear: '2003'},
-    {title:'High Speed Sewer', company: 'Nana minda`s patahian ', startYear: '2002', endYear: '2003'},
-    {title:'Poultry Boy', company: 'Not available', startYear: '2002', endYear: '2003'}
+    {title:'BSIT Student', company: 'Bulacan Agricultural State College', startYear: '2022', endYear: 'present'},
+    {title:'Glass and Aluminum installation helper', company: 'Family Owned Small Business', startYear: '2020', endYear: 'present'},
+    {title:'High Speed Sewer', company: 'Nana minda`s patahian ', startYear: '2017', endYear: '2020'},
+    {title:'Poultry Boy', company: 'Not available', startYear: 'One', endYear: 'Week'}
 ]
