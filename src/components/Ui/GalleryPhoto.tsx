22103d1e95f2cb429f46e21bd8ccaf90655ca059
Zaido <jpmalbas4@gmail.com>
2026-04-14 14:40:31 +0800

    style:Updates on gallery sections

--- src/components/Ui/GalleryPhoto.tsx
+++ src/components/Ui/GalleryPhoto.tsx
@@ -2,7 +2,7 @@ import { type GalleryPhotoProps } from "../../interface/types";
 
 const GalleryPhotos = ({src, alt}:GalleryPhotoProps) => {
     return (
-        <div className="h-70 md:h-45 w-100 md:w-70">
+        <div className="w-md aspect-square rounded-xl overflow-hidden md:h-45 w-100 md:w-70">
             <img className="h-full w-full object-cover transition duration-200 ease-in-out hover:scale-108" src={src} alt={alt} />
         </div>
     );
