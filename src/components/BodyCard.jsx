29fdde2fccccc0f115bf504604b156aa08874a5e
Zaido <jpmalbas4@gmail.com>
2026-04-14 10:46:05 +0800

    style:WIP Mobile responsiveness for header

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -1,7 +1,7 @@
 const BodyCard = (props) => {
     return (
         /* Card Wrapper: Defines the size and style of the card */
-        <div className="w-fit mx-auto my-10 p-4 shadow-lg flex justify-center rounded-sm overflow-x-hidden"> 
+        <div className="w-fit mx-auto md:my-10 p-4 shadow-lg flex justify-center rounded-sm overflow-x-hidden"> 
             <div className="p-1">
                 {props.children}
             </div>
