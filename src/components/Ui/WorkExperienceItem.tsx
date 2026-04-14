9a069514bf84feda2a2a6e9e6b903ccbd3a4ad53
Zaido <jpmalbas4@gmail.com>
2026-04-14 15:24:23 +0800

    style:Style updates

--- src/components/Ui/WorkExperienceItem.tsx
+++ src/components/Ui/WorkExperienceItem.tsx
@@ -9,7 +9,7 @@ const WorkExperienceItem = ({
 }: WorkExperienceItemProps) => {
   return (
     <HoverCards>
-      <div className="flex flex-col gap-1 p-1 rounded-md w-xs md:w-md">
+      <div className="flex flex-col md:gap-1 p-1 rounded-md w-xs md:w-md">
         <strong className="p-1 text-lg md:text-md">{workTitle}</strong>
         <div className="flex justify-between gap-1 p-1">
           <p className="text-md md:text-sm">{workCompany}</p>
