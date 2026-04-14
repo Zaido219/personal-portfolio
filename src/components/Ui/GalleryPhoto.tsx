b5ea5f55b3756180539aa9b074ef664b1c938ffc
Zaido <jpmalbas4@gmail.com>
2026-04-14 14:42:02 +0800

    feat:Official images for the portfolio

--- src/components/Ui/GalleryPhoto.tsx
+++ src/components/Ui/GalleryPhoto.tsx
@@ -2,7 +2,7 @@ import { type GalleryPhotoProps } from "../../interface/types";
 
 const GalleryPhotos = ({src, alt}:GalleryPhotoProps) => {
     return (
-        <div className="w-md aspect-square rounded-xl overflow-hidden md:h-45 w-100 md:w-70">
+        <div className="w-md md:w-xs aspect-square rounded-xl overflow-hidden">
             <img className="h-full w-full object-cover transition duration-200 ease-in-out hover:scale-108" src={src} alt={alt} />
         </div>
     );
