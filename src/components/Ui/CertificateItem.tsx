01f1710a634244cfcd4fd147072ca52d523d84ab
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-05-01 07:46:44 -0700

    feat:WIP event for showing a certificate modal

--- src/components/Ui/CertificateItem.tsx
+++ src/components/Ui/CertificateItem.tsx
@@ -1,8 +1,14 @@
 import type {CertificateItemProps} from "../../interface/types"
 
-const CertificateItem = ({certificateTitle, certificateProvider} : CertificateItemProps) => {
+const showModal = () => {
+    return console.log('Event fired');
+}
+
+const CertificateItem = ({certificateTitle, certificateProvider, onClick} : CertificateItemProps) => {
     return(
-        <div className="flex flex-col gap-1 bg-gray-100 p-1 transition duration-200 ease-in-out hover:scale-103 hover:bg-gray-300 rounded-lg">
+        <div 
+            className="flex flex-col gap-1 bg-gray-100 p-1 transition duration-200 ease-in-out hover:scale-103 hover:bg-gray-300 rounded-lg"
+            onClick={showModal}>
             <p className="font-bold">{certificateTitle}</p>
             <p>{certificateProvider}</p>
         </div>
