c08200395850bf74ed47c5bf4798a95e8ada4257
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-06-27 20:32:27 -0700

    feat:Updated project section

--- src/components/Ui/ProjectItem.tsx
+++ src/components/Ui/ProjectItem.tsx
@@ -1,9 +1,10 @@
 import { type ProjectItemProps} from "../../interface/types"
 
-const ProjectItem = ({projectName, projectDescription, projectLink, ...props} : ProjectItemProps) => {
+const ProjectItem = ({projectName, projectDescription, projectLink,projectStatus, ...props} : ProjectItemProps) => {
     return (
         <div className="h-fit p-2 bg-gray-100 rounded-xl flex flex-col gap-1 transition duration-200 ease-in-out hover:scale-105 hover:bg-gray-300">
             <strong className="text-md">{projectName}</strong>
+            <p className="text-md">{projectStatus}</p>
             <p className="text-sm">{projectDescription}</p>
             <div className="cursor-pointer">
                 <a className="text-xs bg-gray-200 p-1 rounded-lg" href={projectLink}>View Project</a>
