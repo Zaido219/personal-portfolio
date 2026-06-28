a47159470de1bc6ed3b2eb76731ae5de7d886143
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-06-27 23:15:29 -0700

    style:WIP Styling update/s for the Header section

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -12,10 +12,10 @@ const ProfileHeader = () => {
       <div className="flex flex-col md:flex-row items-center p-2">
         <section className="">
           {/* image goes here */}
-          <div className="rounded-full h-50 w-50  md:rounded-sm md:h-43 md:w-43 overflow-hidden">
+          <div className="rounded-full h-54 w-54  md:rounded-sm md:h-43 md:w-43 overflow-hidden">
             <img
               className="h-full w-full object-cover"
-              src="/images/my_pfp_new.png"
+              src="/images/my_pfp_new.jpg"
               alt="my image"
             />
           </div>
