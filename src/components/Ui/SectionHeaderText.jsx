b13d5f534cb9a8d326870eaddc77eb54ea31c42f
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-06-28 04:55:42 -0700

    style:Style update/s for experiences section

--- src/components/Ui/SectionHeaderText.jsx
+++ src/components/Ui/SectionHeaderText.jsx
@@ -1,7 +1,7 @@
 const SectionHeader = ({title="Default Header Title"}) => {
     return (
         <header className="w-fit p-1 m-2 self-start">
-            <h2 className="text-xl md:text-lg font-semibold tracking-tight"> {title} </h2>
+            <h2 className="text-md md:text-2xl font-semibold tracking-tight"> {title} </h2>
         </header>
     )
 }
