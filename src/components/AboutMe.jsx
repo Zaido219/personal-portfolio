b7edb8ed258e54262d23fc85cf8658c8e14eeff4
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-06-27 22:25:24 -0700

    style:WIP Styling update/s for the About me section

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,33 +4,30 @@ import SectionHeader from "./Ui/SectionHeaderText";
 
 const AboutMe = () => {
   return (
-    <BaseSection>
+    <div>
       <SectionHeader title="About" />
-      <section className="">
-        <BaseCard>
-          <div className="bg-red-100 flex flex-col gap-2 w-full">
-            <p className="className=p-1 rounded-sm text-sm md:text-base break-words whitespace-normal">
-              Im an aspiring software engineer and BSIT student at Bulacan
-              Agricultural State College, currently based in San Ildefonso,
-              Bulacan. My passion lies in backend development and systems
-              architecture—specifically building clean, modular solutions that
-              solve real-world problems. My most significant technical
-              contribution to date has been serving as the Lead Developer for
-              GAD Insight, a data analytics system for my college. This role
-              allowed me to bridge the gap between high-level architectural
-              strategy and hands-on implementation using Python, Django, and
-              MySQL. I pride myself on being a "technical craftsman" who values
-              SOLID principles and thorough documentation, ensuring that the
-              systems I build are not just functional, but maintainable for
-              years to come. I am currently seeking a full-time role or
-              growth-oriented opportunity where I can apply my experience in
-              backend logic and system design to help a team build impactful
-              software.
-            </p>
-          </div>
-        </BaseCard>
+      <section className="flex items-center justify-center p-4 min-h-fit">
+        <div className="p-2 rounded-xl w-full max-w-2xl">
+          <p className="text-sm font-normal md:text-base leading-relaxed text-slate-800 break-words whitespace-normal text-left">
+            Im an aspiring software engineer and BSIT student at Bulacan
+            Agricultural State College, currently based in San Ildefonso,
+            Bulacan. My passion lies in backend development and systems
+            architecture—specifically building clean, modular solutions that
+            solve real-world problems. My most significant technical
+            contribution to date has been serving as the Lead Developer for GAD
+            Insight, a data analytics system for my college. This role allowed
+            me to bridge the gap between high-level architectural strategy and
+            hands-on implementation using Python, Django, and MySQL. I pride
+            myself on being a "technical craftsman" who values SOLID principles
+            and thorough documentation, ensuring that the systems I build are
+            not just functional, but maintainable for years to come. I am
+            currently seeking a full-time role or growth-oriented opportunity
+            where I can apply my experience in backend logic and system design
+            to help a team build impactful software.
+          </p>
+        </div>
       </section>
-    </BaseSection>
+    </div>
   );
 };
 
--- src/components/Ui/BaseSection.jsx
+++ src/components/Ui/BaseSection.jsx
@@ -1,6 +1,6 @@
 const BaseSection = ({children}) => {
     return(
-        <div className="min-w-sm p-2 m-2 shadow-sm flex flex-col items-center">
+        <div className="min-w-sm p-2 m-2 shadow-sm flex flex-col items-center overflow-hidden">
             {children}
         </div>
         );
