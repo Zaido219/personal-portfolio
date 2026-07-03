85c524c67eeadeac0bc56c4d48d8812657e41e8b
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-07-03 02:07:08 -0700

    style:WIP dark mode support for the certifications section

--- src/components/Ui/CertificateItem.tsx
+++ src/components/Ui/CertificateItem.tsx
@@ -11,8 +11,8 @@ const CertificateItem = ({certificateTitle, certificateProvider, onClick} : Cert
         <div 
             onClick={onClick}
             className="flex flex-col gap-1 bg-gray-100 p-1 transition duration-200 ease-in-out hover:scale-103 hover:bg-gray-300 rounded-lg">
-            <p className="font-bold">{certificateTitle}</p>
-            <p>{certificateProvider}</p>
+            <p className="font-bold dark:text-black">{certificateTitle}</p>
+            <p className="dark:text-black">{certificateProvider}</p>
         </div>
     );
 };
