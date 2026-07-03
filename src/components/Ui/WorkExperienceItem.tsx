a3b925028b92bfbd395c664139323ff0523b3f54
Artifice-219 <johnphilliplormalbas@gmail.com>
2026-07-03 03:27:38 -0700

    style:style update/s for the work experience section

--- src/components/Ui/WorkExperienceItem.tsx
+++ src/components/Ui/WorkExperienceItem.tsx
@@ -9,12 +9,12 @@ const WorkExperienceItem = ({
 }: WorkExperienceItemProps) => {
   return (
     <HoverCards>
-      <div className="flex flex-col md:gap-1 p-1 rounded-md w-xs md:w-md">
-        <strong className="p-1 text-md md:text-md dark:text-black">{workTitle}</strong>
+      <div className="flex flex-col md:gap-1 p-1 rounded-md w-xs md:w-md dark:bg-zinc-900">
+        <strong className="p-1 text-md md:text-md dark:text-white">{workTitle}</strong>
         <div className="flex justify-between gap-1 p-1">
-          <p className="text-sm md:text-sm dark:text-black">{workCompany}</p>
+          <p className="text-sm md:text-sm dark:text-white">{workCompany}</p>
           <div className="text-sm md:text-xs flex justify-between gap-2">
-            <span className="bg-gray-200 text-xs md:text-xs p-1 rounded-md dark:text-black">
+            <span className="bg-gray-200 dark:bg-zinc-800 text-xs md:text-xs p-1 rounded-md dark:text-white">
               {startDate} - {endDate}
             </span>
           </div>
