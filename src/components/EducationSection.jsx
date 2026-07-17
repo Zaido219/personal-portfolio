4d42a34986224a09e3058815b0bad8e62b48703d
Zaido219 <jpmalbas4@gmail.com>
2026-07-17 13:16:09 +0800

    style:Introduce custom scroll bar

--- src/components/EducationSection.jsx
+++ src/components/EducationSection.jsx
@@ -8,7 +8,7 @@ const EducationExperienceSection = () => {
   return (
     <div>
      <SectionHeader title="Educations"></SectionHeader>
-    <div className="flex flex-col items-center max-h-100 md:max-h-130 overflow-hidden overflow-y-auto">
+    <div className="flex flex-col items-center max-h-100 md:max-h-130 overflow-hidden overflow-y-auto custom-scrollbar">
       <div className="flex flex-col gap-3 p-3 min-w-full items-center">
         {educationExperiences.map((item, index) => {
           return <EducItems key={item.id || index} {...item}></EducItems>;
--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -13,7 +13,7 @@ const ProjectCard = () => {
           <div className="w-84 flex gap-20 items-center">
             <SectionHeader title="Recent Projects" />
           </div>
-          <ul className="max-h-120 md:max-h-200 overflow-y-auto">
+          <ul className="max-h-120 md:max-h-200 overflow-y-auto custom-scrollbar">
               {/* iterate over projects - create project item for each */}
               {
                 Projects.map((item, index) => {
