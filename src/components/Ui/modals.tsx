dc8b87119c37c0930992a2e27425007cbb2cbe74
Zaido219 <jpmalbas4@gmail.com>
2026-07-17 12:51:27 +0800

    style:WIP certificate modal mobile enhancement

--- src/components/Ui/modals.tsx
+++ src/components/Ui/modals.tsx
@@ -21,12 +21,12 @@ export const CertificateModal = ({title, src, isOpen, onClose}  : CertificateMod
             onClose={onClose} // Syncs native 'Esc' key press with React state
             className="fixed inset-0 m-auto rounded-lg p-0 backdrop:bg-black/50"
         >
-            <div className="p-6 bg-purple-200 flex flex-col gap-4">
+            <div className="p-6 bg-white flex flex-col gap-4">
                 <h2 className="text-xl font-bold">{title}</h2>
                 <img src={src} alt={title} className="max-w-full h-auto" />
                 <button 
                     onClick={onClose}
-                    className="mt-4 px-4 py-2 bg-red-500 text-white rounded"
+                    className="mt-4 px-5 py-2.5 bg-slate-800 hover:bg-slate-800 active:scale-[0.98] text-white text-md font-semibold rounded-lg shadow-sm transition-all duration-200 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-900"
                 >
                     Close
                 </button>
