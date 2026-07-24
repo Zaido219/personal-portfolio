18831d67c2aca60a62f9ca6587800bda09875a9a
Zaido219 <jpmalbas4@gmail.com>
2026-07-24 13:08:56 +0800

    style:WIP Updating font sizes for all sections

--- src/components/Ui/WorkExperienceItem.tsx
+++ src/components/Ui/WorkExperienceItem.tsx
@@ -10,11 +10,11 @@ const WorkExperienceItem = ({
   return (
     <HoverCards>
       <div className="flex flex-col md:gap-1 p-1 rounded-md w-xs md:w-md dark:text-zinc-300">
-        <strong className="p-1 text-md md:text-md dark:text-zinc-300">{workTitle}</strong>
+        <strong className="p-1 text-md md:text-lg dark:text-zinc-300">{workTitle}</strong>
         <div className="flex justify-between gap-1 p-1">
-          <p className="text-sm md:text-sm dark:text-zinc-300">{workCompany}</p>
-          <div className="text-sm md:text-xs flex justify-between gap-2">
-            <span className="flex justify-center align-items-center md:min-w-[90px] bg-gray-200 dark:bg-zinc-800 text-xs md:text-xs p-1 rounded-md dark:text-zinc-300">
+          <p className="text-sm md:text-md dark:text-zinc-300">{workCompany}</p>
+          <div className="text-sm md:text-sm flex justify-between gap-2">
+            <span className="flex justify-center align-items-center md:min-w-[90px] bg-gray-200 dark:bg-zinc-800 text-xs md:text-sm p-1 rounded-md dark:text-zinc-300">
               {startDate} - {endDate}
             </span>
           </div>
