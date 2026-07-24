91b05155ec77bf7b75674b971f273146211aeae6
Zaido219 <jpmalbas4@gmail.com>
2026-07-24 11:05:53 +0800

    style:WIP style updates for the footer section

--- src/components/FooterSection.tsx
+++ src/components/FooterSection.tsx
@@ -7,7 +7,9 @@ export const Footer = () => {
       <div className="">
         <div>{/* some icon will go in here */}</div>
         <div>
-          <p className="text-md">Let me help you build solutions that will last a lifetime.</p>
+          <p className="text-xl">
+            Let me help you build solutions that will last a lifetime.
+          </p>
         </div>
         <div className="flex justify-around">
           <div>
@@ -51,6 +53,34 @@ export const Footer = () => {
             >
               <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
                 <path d="M19 0h-14c-2.761 0-5 2.239-5 5v14c0 2.761 2.239 5 5 5h14c2.762 0 5-2.239 5-5v-14c0-2.761-2.238-5-5-5zm-11 19h-3v-11h3v11zm-1.5-12.268c-.966 0-1.75-.79-1.75-1.764s.784-1.764 1.75-1.764 1.75.79 1.75 1.764-.783 1.764-1.75 1.764zm13.5 12.268h-3v-5.604c0-3.368-4-3.113-4 0v5.604h-3v-11h3v1.765c1.396-2.586 7-2.777 7 2.476v6.759z" />
+              </svg>
+            </a>
+          </div>
+          <div>
+            <div>
+              {/* Viber Icon Link */}
+              <a
+                href="viber://chat?number=%2B639123456789" // Replace with your phone number (international format without + or spaces)
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Viber"
+                className="text-neutral-400 hover:text-white transition-colors"
+              >
+                <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+                  <path d="M20.2 16.2c-.6-.4-2.8-1.4-3.2-1.5-.4-.1-.8.2-1.1.6s-1.1 1.4-1.3 1.6c-.2.2-.5.2-.8 0-.4-.2-1.8-.7-3.4-2.1-1.3-1.1-2.1-2.5-2.4-3-.2-.4 0-.7.2-.9.2-.2.4-.5.6-.7.2-.2.3-.4.4-.7.1-.2 0-.5-.1-.7-.1-.2-.9-2.2-1.3-3-.3-.8-.7-.7-1-.7h-.8c-.3 0-.9.1-1.3.6S3 7.8 3 9.9c0 2.1 1.5 4.1 1.7 4.4.2.3 3 4.6 7.3 6.4 1 .4 1.8.7 2.4.9.9.3 1.8.3 2.5.2.8-.1 2.5-1 2.8-2 .3-1 .3-1.8.2-2s-.3-.3-.7-.5zM14.2 3.2c3.9.5 7 3.6 7.5 7.5.1.5-.3 1-.8 1.1h-.1c-.5 0-.9-.4-1-.8-.4-3.1-2.8-5.5-5.9-5.9-.5-.1-.9-.5-.8-1 0-.5.4-.9.9-.9zM14.5 7.2c2 .4 3.6 2 4 4 .1.5-.2 1-.7 1.1h-.2c-.4 0-.8-.3-.9-.7-.3-1.2-1.2-2.1-2.4-2.4-.5-.1-.8-.5-.7-1 .1-.5.5-.8 1-.6z" />
+                </svg>
+              </a>
+            </div>
+          </div>
+          <div>
+            {/* Email Icon Link */}
+            <a
+              href="mailto:jpmalbas4@gmail.com"
+              aria-label="Email"
+              className="text-neutral-400 hover:text-white transition-colors"
+            >
+              <svg className="w-5 h-5 fill-current" viewBox="0 0 24 24">
+                <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 4l-8 5-8-5V6l8 5 8-5v2z" />
               </svg>
             </a>
           </div>
