71aea551f9e760333a07959d2b1367d0af8e68db
Zaido219 <jpmalbas4@gmail.com>
2026-07-31 13:59:08 +0800

    feat:WIP implementing tech stack section

--- src/config/TechStacks.tsx
+++ src/config/TechStacks.tsx
@@ -1,10 +1,18 @@
 import type { TechStackItem } from "../interface/types";
+import { 
+  SiSharp, 
+  SiDotnet, 
+  SiReact, 
+  SiTypescript, 
+  SiTailwindcss, 
+  SiPostgresql 
+} from "@icons-pack/react-simple-icons";
 
 export const TechStacks : TechStackItem[] = [
     {
         field:'backend',
         name:'C#',
-        icon:
-        color:
+        icon:<SiSharp/>,
+        color:"#512BD4",
     },
 ]
