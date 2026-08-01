64915171302d62c0a22fa9f51d7329828c44c093
Zaido219 <jpmalbas4@gmail.com>
2026-08-01 13:25:15 +0800

    style:WIP Style update for the tech stack group card

--- src/components/Ui/TechStackItem.tsx
+++ src/components/Ui/TechStackItem.tsx
@@ -2,7 +2,7 @@ import { type TechStackItemProps} from "./../../interface/types";
 
 const TechStackItem = ({ name, icon, color }: TechStackItemProps) => {
   return (
-    <div className="flex items-center gap-3 p-1">
+    <div className="flex flex-col items-center gap-3 p-1">
       {/* Apply the passed color string here */}
       <span 
         className="text-xl flex-shrink-0 flex items-center justify-center [&>svg]:w-6 md:[&>svg]:w-8 [&>svg]:h-6 md:[&>svg]:h-8"
