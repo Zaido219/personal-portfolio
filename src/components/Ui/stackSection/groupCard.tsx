717c59eb35fc64c01a3cf0947bee488ea02c11cd
Zaido219 <jpmalbas4@gmail.com>
2026-08-01 13:58:07 +0800

    style:WIP Style update for the tech stack group card

--- src/components/Ui/stackSection/groupCard.tsx
+++ src/components/Ui/stackSection/groupCard.tsx
@@ -2,7 +2,7 @@ import { type groupCardProps } from "../../../interface/types";
 
 const GroupCard = ({ children }:groupCardProps) => {
   return (
-    <div className="space-y-2 p-2 md:shadow-sm shadow-md rounded-lg">
+    <div className="space-y-2 p-2 md:shadow-sm shadow-md rounded-lg dark:bg-zinc-800">
         {children}
     </div>
   );
