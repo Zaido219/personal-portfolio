caa95a2fc2ffb4c272541e51da51c6f52c91715d
Zaido219 <jpmalbas4@gmail.com>
2026-08-09 11:09:07 +0800

    style:Added a grid background

--- src/components/BodyCard.jsx
+++ src/components/BodyCard.jsx
@@ -1,6 +1,7 @@
 const BodyCard = (props) => {
     return (
-        <div className="max-w-sm md:max-w-7xl mx-auto md:my-10 p-4 shadow-lg flex justify-center rounded-sm overflow-x-hidden"> 
+        <div className="relative max-w-sm md:max-w-7xl mx-auto md:my-10 p-4 shadow-lg flex justify-center rounded-sm overflow-x-hidden"> 
+        <div className="absolute inset-0 bg-grid-pattern -z-10 pointer-events-none"></div>
             <div className="p-1">
                 {props.children}
             </div>
