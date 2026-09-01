4a7cb35e29867a338cdc007e712cf58bd3d402d2
Zaido219 <jpmalbas4@gmail.com>
2026-09-01 22:24:54 +0800

    style: wip style update for the certificate section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,5 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { HeroSection } from "./components/HeroSection/ProfileHeader";
-import BodyCard from "./components/BodyCard";
-import ProjectCard from "./components/ProjectCard";
-import Recognition from "./components/Recognition";
-import WorkExperienceSection from "./components/WorkExperience";
-import Gallery from "./components/GallerySection";
-import EducationExperienceSection from "./components/EducationSection";
 import { FeaturedWorkSection } from "./components/FeaturedWorkSection/FeaturedWork";
 import { ExperienceSection } from "./components/ExperiencesSection/ExperienceSection";
 import {Header} from "./components/HeroSection/ProfileHeader";
