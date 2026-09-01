87af5924357aab95037f5028a9865f05a7136302
Zaido219 <jpmalbas4@gmail.com>
2026-09-01 22:29:15 +0800

    style: wip adding better background to highlight glassmorphic effects

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import "./App.css";
-import { HeroSection } from "./components/HeroSection/ProfileHeader";
+import { Header, HeroSection } from "./components/HeroSection/ProfileHeader";
 import { FeaturedWorkSection } from "./components/FeaturedWorkSection/FeaturedWork";
 import { ExperienceSection } from "./components/ExperiencesSection/ExperienceSection";
-import {Header} from "./components/HeroSection/ProfileHeader";
 import StackSection from "./components/TechStackSection/TechStackSection";
 import CertificatesSection from "./components/CertificateSection/CertificateSection";
 import AboutSection from "./components/AboutSection/AboutMe";
@@ -10,16 +9,33 @@ import Footer from "./components/FooterSection/FooterSection";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <HeroSection />
-      <FeaturedWorkSection />
-      <ExperienceSection />
-      <StackSection/>
-      <CertificatesSection/>
-      <AboutSection/>
-      <Footer/>
-    </>
+    <div className="relative min-h-screen bg-neutral-950 text-neutral-100 overflow-x-hidden">
+      {/* Fixed Sunset Ambient Mesh Layer */}
+      <div className="pointer-events-none fixed inset-0 z-0 overflow-hidden">
+        {/* Top-Right Orb */}
+        <div className="absolute -top-40 -right-40 w-[600px] h-[600px] rounded-full bg-gradient-to-br from-[#EA6113]/30 to-[#F88F22]/20 blur-[120px]" />
+        
+        {/* Mid-Left Sunset Dusk Orb */}
+        <div className="absolute top-[35%] -left-40 w-[550px] h-[550px] rounded-full bg-gradient-to-tr from-[#B84305]/25 to-[#FBB931]/15 blur-[140px]" />
+        
+        {/* Bottom Accent Orb */}
+        <div className="absolute bottom-10 right-10 w-[450px] h-[450px] rounded-full bg-gradient-to-tl from-[#FFE3B3]/10 to-[#EA6113]/20 blur-[130px]" />
+      </div>
+
+      {/* Main Page Layout Layer */}
+      <div className="relative z-10">
+        <Header />
+        <main>
+          <HeroSection />
+          <FeaturedWorkSection />
+          <ExperienceSection />
+          <StackSection />
+          <CertificatesSection />
+          <AboutSection />
+        </main>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
